2b267b7c1de5bc557c8e3252432ef6dc29dda85d
majocampos92 <maria_campos84@hotmail.com>
2025-02-28 00:29:16 -0600

    feat: added header section at home

--- RickAndMorty/Presentation/Screens/Home/HomeView.swift
+++ RickAndMorty/Presentation/Screens/Home/HomeView.swift
@@ -13,41 +13,52 @@ struct HomeView: View {
     
     var body: some View {
         GeometryReader { geometry in
-            //MARK: Characters Section
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(viewModel.characters, id: \.id) { character in
-                        VStack(alignment: .leading, spacing: 8) {
-                            KFImage(URL(string: character.image)!)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(
-                                    width: geometry.size.width * 0.30,
-                                    height: geometry.size.height * 0.15
-                                )
-                                .clipped()
-                            
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(character.name)
+            VStack {
+            }
+            .frame(
+                width: geometry.size.width,
+                height: geometry.size.height * 0.15,
+                alignment: .top
+            )
+            .background(Color("deep_navy"))
+            .ignoresSafeArea()
+            VStack {
+                //MARK: Characters Section
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(viewModel.characters, id: \.id) { character in
+                            VStack(alignment: .leading, spacing: 8) {
+                                KFImage(URL(string: character.image)!)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(
+                                        width: geometry.size.width * 0.30,
+                                        height: geometry.size.height * 0.15
+                                    )
+                                    .clipped()
                                 
-                                Text(character.species)
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(character.name)
+                                    
+                                    Text(character.species)
+                                }
+                                .padding()
                             }
-                            .padding()
+                            .frame(
+                                width: geometry.size.width * 0.30,
+                                height: geometry.size.height * 0.25,
+                                alignment: .leading
+                            )
+                            .background(.white)
+                            .cornerRadius(15)
+                            .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 3)
                         }
-                        .frame(
-                            width: geometry.size.width * 0.30,
-                            height: geometry.size.height * 0.25,
-                            alignment: .leading
-                        )
-                        .background(.white)
-                        .cornerRadius(15)
-                        .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 3)
                     }
-                }
-                .padding(.horizontal, 10)
-                .padding(.vertical, 10)
-                .onAppear {
-                    viewModel.fecthCharacters()
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 10)
+                    .onAppear {
+                        viewModel.fecthCharacters()
+                    }
                 }
             }
         }
